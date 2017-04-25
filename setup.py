fa5820ac25ab30df7307094ac78173a96c3d7034
Ed Parcell <edparcell@gmail.com>
2017-04-24 23:58:46 -0600

    Update version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='loman',
-    version='0.1.0',
+    version='0.1.1',
     packages=['loman'],
     url='https://github.com/janusassetallocation/loman',
     license='BSD',
