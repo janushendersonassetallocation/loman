c5ddaf3c124f4d210db50b7f367671f7e9ef6e48
Ed Parcell <edparcell@gmail.com>
2024-08-22 23:27:03 -0600

    Update to version 0.4.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='loman',
-    version='0.3.0',
+    version='0.4.0',
     packages=['loman'],
     url='https://github.com/janusassetallocation/loman',
     license='BSD',
@@ -10,13 +10,16 @@ setup(
     author_email='edparcell@gmail.com',
     description='Loman tracks state of computations, and the dependencies between them, allowing full and partial recalculations.',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12'
     ],
     install_requires=['decorator', 'dill', 'pydotplus', 'networkx', 'pandas', 'matplotlib'],
     extras_require={
