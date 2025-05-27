c4c8d3d4b188f48945d49f6b7f8a52b9950a10b7
Ed Parcell <edparcell@gmail.com>
2025-05-27 10:15:30 -0600

    Add NodeKey classes and functions to loman.__init__.py

--- loman/__init__.py
+++ loman/__init__.py
@@ -2,6 +2,7 @@ from loman.computeengine import (Computation, ComputationFactory, node, C, input
 from loman.exception import (MapException, LoopDetectedException, NonExistentNodeException,
                              CannotInsertToPlaceholderNodeException)
 from loman.consts import States, NodeTransformations
-import loman.visualization as visualization
+import loman.visualization as viz
 from loman.visualization import GraphView
 import loman.util as util
+from loman.nodekey import NodeKey, Names, Name, to_nodekey
